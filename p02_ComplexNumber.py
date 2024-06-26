27a2e5e819bade053f4b94acea163fd92f3c7514
Petr Gregor <petr.gregor@email.cz>
2024-06-26 09:31:21 +0200

    ComplexNumber tests.

--- p02_ComplexNumber.py
+++ p02_ComplexNumber.py
@@ -1,5 +1,5 @@
 
-class ComplexNumber:
+"""class ComplexNumber:
     __re = None
     __img = None
 
@@ -36,3 +36,91 @@ class ComplexNumber:
 
     def add(self, other):
         return ComplexNumber(self.re + other.re, self.img + other.img)
+"""
+
+
+class ComplexNumber:
+    __re = 0
+    __img = 0
+
+    def __init__(self, real_part=0.0, img_part=0.0):
+        self.re = real_part
+        self.img = img_part
+
+    def __eq__(self, other):
+        return self.re == other.re and self.img == other.img
+
+    def __lt__(self, other):
+        return self.absolute_value() < other.absolute_value()
+
+    def __gt__(self, other):
+        return self.absolute_value() > other.absolute_value()
+
+    def __repr__(self):
+        return f"ComplexNumber(re={self.re}, img={self.img})"
+
+    def __str__(self):
+        if self.re == 0 and self.img == -1:
+            return f"-i"
+        if self.re == 0 and self.img != 0:
+            return f"{self.img}i"
+        if self.img == -1:
+            return f"{self.re} - i"
+        if self.img < 0:
+            return f"{self.re} - {-self.img}i"
+        if self.img == 0:
+            return f"{self.re}"
+        return f"{self.re} + {self.img}i"
+
+    @property
+    def re(self):
+        return self.__re
+
+    @re.setter
+    def re(self, real_part):
+        if isinstance(real_part, int) or isinstance(real_part, float):
+            self.__re = real_part
+        else:
+            raise Exception
+
+    @property
+    def img(self):
+        return self.__img
+
+    @img.setter
+    def img(self, img_part):
+        if isinstance(img_part, int) or isinstance(img_part, float):
+            self.__img = img_part
+        else:
+            raise Exception
+
+    def add(self, other):
+        return ComplexNumber(self.re + other.re, self.img + other.img)
+
+    def subtract(self, other):
+        return ComplexNumber(self.re - other.re, self.img - other.img)
+
+    def multiply(self, other):
+        a = self.re
+        b = self.img
+        c = other.re
+        d = other.img
+        return ComplexNumber(a * c - b * d, a * d + b * c)
+
+    def divide(self, other):
+        a = self.re
+        b = self.img
+        c = other.re
+        d = other.img
+        if c != 0 or d != 0:
+            return ComplexNumber((a * c + b * d) / (c ** 2 + d ** 2), (b * c - a * d) / (c ** 2 + d ** 2))
+        else:
+            return None
+
+    def conjugate(self):
+        return ComplexNumber(self.re, -self.img)
+
+    def absolute_value(self):
+        a = self.re
+        b = self.img
+        return (a ** 2 + b ** 2) ** (1 / 2)
--- test02_ComplexNumber.py
+++ test02_ComplexNumber.py
@@ -1,5 +1,9 @@
-import pytest
+"""import pytest
 from p02_ComplexNumber import *
+
+# TODO: test_init
+# TODO: test_re
+# TODO: test_img
 
 
 def test_addition():
@@ -11,7 +15,132 @@ def test_addition():
 # TODO: test_multiply
 # TODO: test_divide
 # TODO: test_conjugate
-# TODO: test_re
-# TODO: test_img
 # TODO: test_absolute_value
 # TODO: test_str
+"""
+
+import pytest
+from p02_ComplexNumber import *
+
+
+def test_init():
+    with pytest.raises(Exception):
+        assert ComplexNumber("Hi", 2)
+        assert ComplexNumber(5, "ahoj")
+        assert ComplexNumber("hello", "world")
+        assert ComplexNumber(4, None)
+        assert ComplexNumber(None, -1)
+        assert ComplexNumber(None, None)
+
+    complex_number1 = ComplexNumber(0, 0)
+    assert complex_number1.re == 0
+    assert complex_number1.img == 0
+
+    complex_number2 = ComplexNumber(real_part=7)
+    assert complex_number2.re == 7
+    assert complex_number2.img == 0
+
+    complex_number3 = ComplexNumber(img_part=5)
+    assert complex_number3.re == 0
+    assert complex_number3.img == 5
+
+    complex_number4 = ComplexNumber(9)
+    assert complex_number4.re == 9
+    assert complex_number4.img == 0
+
+
+def test_re():
+    complex_number1 = ComplexNumber(5, 2)
+    assert complex_number1.re == 5
+    complex_number1.re = 6
+    assert complex_number1.re == 6
+    with pytest.raises(Exception):
+        complex_number1.re = "Hi"
+        complex_number1.re = None
+        complex_number1.re = []
+
+
+def test_img():
+    complex_number1 = ComplexNumber(5, 2)
+    assert complex_number1.img == 2
+    complex_number1.img = -1
+    assert complex_number1.img == -1
+    with pytest.raises(Exception):
+        complex_number1.img = "Hi"
+        complex_number1.img = None
+
+
+def test_str():
+    complex_number1 = ComplexNumber(3, 2)
+    complex_number2 = ComplexNumber(4, -1)
+    complex_number3 = ComplexNumber(-4, -2)
+    complex_number4 = ComplexNumber(4, 0)
+    complex_number5 = ComplexNumber(0, -3)
+    complex_number6 = ComplexNumber(0, 0)
+    complex_number7 = ComplexNumber(0, -1)
+    assert complex_number1.__str__() == "3 + 2i"
+    assert complex_number2.__str__() == "4 - i"
+    assert complex_number3.__str__() == "-4 - 2i"
+    assert complex_number4.__str__() == "4"
+    assert complex_number5.__str__() == "-3i"
+    assert complex_number6.__str__() == "0"
+    assert complex_number7.__str__() == "-i"
+
+
+def test_addition():
+    complex_number1 = ComplexNumber(3, 2)
+    complex_number2 = ComplexNumber(1, -1)
+    assert complex_number1.add(complex_number2) == ComplexNumber(4, 1)
+
+
+def test_subtraction():
+    complex_number1 = ComplexNumber(3, 2)
+    complex_number2 = ComplexNumber(1, -1)
+    assert complex_number1.subtract(complex_number2) == ComplexNumber(2, 3)
+
+
+def test_multiply():
+    complex_number1 = ComplexNumber(3, 2)
+    complex_number2 = ComplexNumber(4, -1)
+    assert complex_number1.multiply(complex_number2) == ComplexNumber(14, 5)
+
+
+def test_divide():
+    complex_number1 = ComplexNumber(3, 2)
+    complex_number2 = ComplexNumber(4, -1)
+    complex_number3 = ComplexNumber(-2, -1)
+    complex_number4 = ComplexNumber(0, 0)
+    assert complex_number1.divide(complex_number2) == ComplexNumber(10 / 17, 11 / 17)
+    assert complex_number3.divide(complex_number4) is None
+
+
+def test_conjugate():
+    complex_number1 = ComplexNumber(3, 2)
+    complex_number2 = ComplexNumber(4, -1)
+    assert complex_number1.conjugate() == ComplexNumber(3, -2)
+    assert complex_number2.conjugate() == ComplexNumber(4, 1)
+    assert complex_number1.multiply(complex_number1.conjugate()) == ComplexNumber((3 ** 2) + (2 ** 2), 0)
+    assert complex_number2.multiply(complex_number2.conjugate()) == ComplexNumber((4 ** 2) + (1 ** 2), 0)
+
+
+def test_absolute_value():
+    complex_number1 = ComplexNumber(3, 2)
+    complex_number2 = ComplexNumber(4, -1)
+    assert complex_number1.absolute_value() == 13 ** (1 / 2)
+    assert complex_number2.absolute_value() == 17 ** (1 / 2)
+    complex_number3 = ComplexNumber(2)
+    complex_number4 = ComplexNumber(img_part=5)
+    assert complex_number3.absolute_value() == 2
+    assert complex_number4.absolute_value() == 5
+    complex_number5 = ComplexNumber(0)
+    assert complex_number5.absolute_value() == 0
+    complex_number6 = ComplexNumber(-9)
+    assert complex_number6.absolute_value() == 9
+    complex_number7 = ComplexNumber(-0.0001)
+    assert complex_number7.absolute_value() == 0.0001
+
+
+def test_lt():
+    complex_number1 = ComplexNumber(2)
+    complex_number2 = ComplexNumber(img_part=5)
+    assert complex_number1 < complex_number2
